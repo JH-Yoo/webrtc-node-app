56751263d775b175de652f3c03fdbdbe0d8e8075
유주현 <gus6359@gmail.com>
2022-07-25 15:08:06 +0900

    web rtc node app init

---
+++ public/client.js
@@ -0,0 +1,154 @@
+const roomSelectionContainer = document.getElementById("room-selection-container");
+const roomInput = document.getElementById("room-input");
+const connectButton = document.getElementById("connect-button");
+const videoChatContainer = document.getElementById("video-chat-container");
+const localVideoComponent = document.getElementById("local-video");
+const remoteVideoComponent = document.getElementById("remote-video");
+
+const socket = io();
+const mediaConstraints = {
+  audio: true,
+  video: { width: 1280, height: 720 }
+}
+let localStream;
+let remoteStream;
+let isRoomCreator;
+let rtcPeerConnection;
+let roomId;
+
+const iceServers = {
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" },
+    { urls: "stun:stun2.l.google.com:19302" },
+    { urls: "stun:stun3.l.google.com:19302" },
+    { urls: "stun:stun4.l.google.com:19302" },
+  ]
+}
+
+connectButton.addEventListener("click", () => {
+  joinRoom(roomInput.value);
+});
+
+// SOCKET EVENT CALL BACK
+socket.on("room_created", async () => {
+  console.log("Socket event call back : room created");
+  await setLocalStream(mediaConstraints);
+  isRoomCreator = true;
+});
+socket.on("room_joined", async () => {
+  console.log("Socket event call back : room joined");
+  await setLocalStream(mediaConstraints);
+  socket.emit("start_call", roomId);
+});
+// socket.on("full_room", () => {});
+
+socket.on("start_call", async () => {
+  console.log("Socket event call back : start_call");
+
+  if (isRoomCreator) {
+    rtcPeerConnection = new RTCPeerConnection(iceServers);
+    addLocalTracks(rtcPeerConnection);
+    rtcPeerConnection.ontrack = setRemoteStream;
+    rtcPeerConnection.onicecandidate = sendIceCandidate;
+    await createOffer(rtcPeerConnection);
+  }
+});
+
+socket.on("webrtc_offer", async (event) => {
+  console.log("Socket event call back : webrtc_offer");
+
+  if (isRoomCreator) {
+    rtcPeerConnection = new RTCPeerConnection(iceServers);
+    addLocalTracks(rtcPeerConnection);
+    rtcPeerConnection.ontrack = setRemoteStream;
+    rtcPeerConnection.onicecandidate = sendIceCandidate;
+    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+    await createAnswer(rtcPeerConnection);
+  }
+});
+
+socket.on("webrtc_answer", async (event) => {
+  console.log("Socket event call back : webrtc_answer");
+  rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+});
+
+socket.on("webrtc_ice_candidate", async (event) => {
+  console.log("Socket event call back : webrtc_ice_candidate");
+  let candidate = new RTCIceCandidate({
+    sdpMLineIndex: event.label,
+    candidate: event.candidate,
+  });
+  rtcPeerConnection.addIceCandidate(candidate);
+});
+
+// UTIL FUNCTIONS
+const joinRoom = (room) => {
+  if (room == '') {
+    alert("Please Input the room id");
+  } else {
+    roomId = room;
+    socket.emit("join", room);
+    showVideoConference();
+  }
+}
+const showVideoConference = () => {
+  roomSelectionContainer.style.display = 'none';
+  videoChatContainer.style.display = 'block';
+}
+const setLocalStream = async (mediaConstraints) => {
+  let stream;
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+  } catch (e) {
+    console.log("show error. " + e);
+  }
+  localStream = stream;
+  localVideoComponent.srcObject = stream;
+}
+const addLocalTracks = (rtcPeerConnection) => {
+  localStream.getTracks().forEach((track) => {
+    rtcPeerConnection.addTrack(track, localStream);
+  });
+}
+const createOffer = async (rtcPeerConnection) => {
+  let sessionDescription;
+  try {
+    sessionDescription = await rtcPeerConnection.createOffer();
+    rtcPeerConnection.setLocalDescription(sessionDescription);
+  } catch (e) {
+    console.log("create offer error : " + e);
+  }
+  socket.emit("webrtc_offer", {
+    type: "webrtc_offer",
+    sdp: sessionDescription,
+    roomId,
+  });
+}
+const createAnswer = async (rtcPeerConnection) => {
+  let sessionDescription;
+  try {
+    sessionDescription = await rtcPeerConnection.createAnswer();
+    rtcPeerConnection.setLocalDescription(sessionDescription);
+  } catch (e) {
+    console.log("create answer error : " + e);
+  }
+  socket.emit("webrtc_answer", {
+    type: "webrtc_answer",
+    sdp: sessionDescription,
+    roomId,
+  });
+}
+const setRemoteStream = (event) => {
+  remoteVideoComponent.srcObject = event.streams[0];
+  remoteStream = event.stream;
+}
+const sendIceCandidate = (event) => {
+  if (event.candidate) {
+    socket.emit("webrtc_ice_candidate", {
+      roomId,
+      label: event.candidate.sdpMLineIndex,
+      candidate: event.candidate.candidate,
+    });
+  }
+}
---
+++ src/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+app.use("/", express.static("public"));
+
+io.on("connection", (socket) => {
+  socket.on("join", (roomId) => {
+    const roomClient = io.sockets.adapter.rooms[roomId] || { length: 0 };
+    const numberOfClient = roomClient.length;
+    if (numberOfClient == 0) {
+      console.log("Create room : " + roomId);
+      socket.join(roomId);
+      socket.emit("room_created", roomId);
+    } else {
+      console.log("Join room : " + roomId);
+      socket.join(roomId);
+      socket.emit("room_joined", roomId);
+    }
+  });
+
+  socket.on("start_call", (roomId) => {
+    console.log("BroadCasting start call event to peers in room : " + roomId);
+    socket.broadcast.to(roomId).emit("start_call");
+  });
+
+  socket.on("webrtc_offer", (event) => {
+    console.log("BroadCasting web rtc offer event to peers in room : " + event.roomId);
+    socket.broadcast.to(event.roomId).emit("webrtc_offer", event);
+  });
+
+  socket.on("webrtc_answer", (event) => {
+    console.log("BroadCasting web rtc answer event to peers in room : " + event.roomId);
+    socket.broadcast.to(event.roomId).emit("webrtc_answer", event);
+  });
+
+  socket.on("webrtc_ice_candidate", (event) => {
+    console.log("BroadCasting web rtc ice candidate event to peers in room : " + event.roomId);
+    socket.broadcast.to(event.roomId).emit("webrtc_ice_candidate", event);
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log("Hello Iam server");
+});
