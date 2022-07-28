10d588e754838346a14cee81f3a729f7fcfe7044
유주현 <gus6359@gmail.com>
2022-07-28 13:59:34 +0900

    Feat: WebSocket package 사용한 server 구현 (client 제외)

---
+++ src/server-ws.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+  if (wss.clients.length > 2) {
+    return;
+  }
+  ws.on('message', (message) => {
+    console.log('received: %s', message);
+    const response = JSON.parse(message);
+    switch (response.type) {
+      case "join":
+        join();
+        break;
+      default:
+        broadcast(response);
+        break;
+    }
+  });
+
+  ws.on('close', (event) => {
+    console.log("disconnect");
+    console.log(event);
+  })
+});
+
+wss.broadcast = (msg) => {
+  wss.clients.forEach((client) => {
+    client.send(msg);
+  });
+};
+
+const port = 3000;
+server.listen(port, () => {
+  console.log("Hello Iam server. port : " + port);
+});
+
+const join = () => {
+  console.log("try join to room");
+  if (wss.clients.size == 1) {
+    broadcast({ type: "room_created" });
+  } else {
+    broadcast({ type: "room_joined" });
+  }
+}
+const broadcast = (data = {}) => {
+  console.log(`BroadCasting ${data.type} event to peers`);
+  wss.broadcast(JSON.stringify(data));
+}
